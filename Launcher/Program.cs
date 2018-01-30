94717ef49316e6ef7a3e836765d7385a75a4ee3a
Marchio Roque M <martinmarchio@hotmail.com>
2018-01-30 17:24:10 -0300

    .

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -12,20 +12,6 @@ namespace Launcher
             ControladorMaestro c = new ControladorMaestro();
 
             var cache = c.ObtenerCacheDeElementos();
-
-            //DateTime fecha = DateTime.Now;
-            //using (var ord = new ControladorOrdenativos())
-            //{
-            //    fecha = ord.Test();
-            //    Console.WriteLine("La Fecha actual es: " + fecha.ToString());
-            //    Console.WriteLine("");
-            //    Console.WriteLine("Preciona ESC");
-
-            //    do
-            //    {
-            //        while (!Console.KeyAvailable){}
-            //    } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
-            //}
         }
     }
 }
